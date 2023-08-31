a46ba15a7b1ac7970a0bc252dc10d1b5f68268d2
bitaraf <en.bitaraf@gmail.com>
2023-08-31 23:05:12 +0330

    fix

--- Entities/DTO/Record/ContactRes.cs
+++ Entities/DTO/Record/ContactRes.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entities.DTO.Record
 {
-    public class ContactRes
+    public record ContactRes
     {
         public int Id { get; set; } 
         public string Name { get; set; }
